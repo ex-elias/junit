3abd7d3d5cf90b22c5768e25e57f6325fe3da414
jexidas <ilshelkov@ya.ru>
2019-12-09 19:57:49 +0300

    Update ArrTest4.java

--- src/test/java/junit/arr/ArrTest4.java
+++ src/test/java/junit/arr/ArrTest4.java
@@ -7,27 +7,23 @@ import org.junit.Test;
 public class ArrTest4 {
 	//Return true if the array contains, somewhere, three increasing adjacent numbers like .... 4, 5, 6, ... or 23, 24, 25.
 
+    private Arr arr = new Arr();
+
 	@Test
 	public void shouldReturnTrueWhenTripleUp() {
 		int[] actual = new int[] {1, 4, 5, 6, 2};
-
-		Arr arr = new Arr();
 		assertTrue(arr.tripleUp(actual));
 	}
 
 	@Test
 	public void shouldReturnFalseWhenNoTripleUp() {
 		int[] actual = new int[] {1, 2, 4, 5, 7, 6, 5, 7, 7, 6};
-
-		Arr arr = new Arr();
 		assertFalse(arr.tripleUp(actual));
 	}
 
 	@Test
 	public void shouldReturnFalseWhenEmpty() {
 		int[] actual = new int[] {};
-
-		Arr arr = new Arr();
 		assertFalse(arr.tripleUp(actual));
 	}
 
@@ -35,8 +31,6 @@ public class ArrTest4 {
 	@Test(expected = NullPointerException.class)
 	public void shouldReturnNullPointerExceptionWhenNull() {
 		int[] actual = null;
-
-		Arr arr = new Arr();
 		arr.tripleUp(actual);
 	}
 
