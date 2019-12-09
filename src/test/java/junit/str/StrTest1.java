40223b0dfd20e18147492e805fae0c5069e33e90
jexidas <ilshelkov@ya.ru>
2019-12-09 19:55:05 +0300

    Update StrTest1.java

--- src/test/java/junit/str/StrTest1.java
+++ src/test/java/junit/str/StrTest1.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class StrTest1 {
 	//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
 
+    private Str str = new Str();
+
 	@Test
 	public void shouldReturnRequiredResultWhenNotEmpty() {
 		String actual = "Bob";
 		String expected = "Hello Bob!";
-
-		Str str = new Str();
 		assertEquals(expected, str.helloName(actual));
 	}
 
@@ -20,8 +20,6 @@ public class StrTest1 {
 	public void shouldReturnRequiredResultWhenEmpty() {
 		String actual = "";
 		String expected = "Hello !";
-
-		Str str = new Str();
 		assertEquals(expected, str.helloName(actual));
 	}
 
@@ -29,8 +27,6 @@ public class StrTest1 {
 	public void shouldReturnRequiredResultWhenNull() {
 		String actual = null;
 		String expected = "Hello null!";
-
-		Str str = new Str();
 		assertEquals(expected, str.helloName(actual));
 	}
 
