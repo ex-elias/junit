99d9abcc33f808fe2569c61475cdb8177c70b7d4
jexidas <ilshelkov@ya.ru>
2019-12-09 19:57:59 +0300

    Update ArrTest5.java

--- src/test/java/junit/arr/ArrTest5.java
+++ src/test/java/junit/arr/ArrTest5.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class ArrTest5 {
 	//This is slightly more difficult version of the famous FizzBuzz problems. (See: https://codingbat.com/prob/p153059)
 
+    private Arr arr = new Arr();
+
 	@Test
 	public void shouldReturnRequiredResultWhenMultiplesOfThreeAndFive() {
 		int[] actual = new int[] {1, 6};
 		String[] expected = {"1", "2", "Fizz", "4", "Buzz"};
-
-		Arr arr = new Arr();
 		assertArrayEquals(expected, arr.fizzBuzz(actual[0], actual[1]));
 	}
 
@@ -20,24 +20,18 @@ public class ArrTest5 {
 	public void shouldReturnRequiredResultWhenNoMultiplesOfThreeAndFive() {
 		int[] actual = new int[] {7, 9};
 		String[] expected = {"7", "8"};
-
-		Arr arr = new Arr();
 		assertArrayEquals(expected, arr.fizzBuzz(actual[0], actual[1]));
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void shouldReturnArrayIndexOutOfBoundsExceptionWhenOnlyOneElement() {
 		int[] actual = new int[] {2};
-
-		Arr arr = new Arr();
 		arr.fizzBuzz(actual[0], actual[1]);
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void shouldReturnArrayIndexOutOfBoundsExceptionWhenEmpty() {
 		int[] actual = new int[] {};
-
-		Arr arr = new Arr();
 		arr.fizzBuzz(actual[0], actual[1]);
 	}
 
@@ -45,8 +39,6 @@ public class ArrTest5 {
 	@Test(expected = NullPointerException.class)
 	public void shouldReturnNullPointerExceptionWhenNull() {
 		int[] actual = null;
-
-		Arr arr = new Arr();
 		arr.fizzBuzz(actual[0], actual[1]);
 	}
 
