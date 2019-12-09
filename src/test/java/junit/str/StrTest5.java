301f509b4025613116b7b96ff1e7c7a5caab994c
jexidas <ilshelkov@ya.ru>
2019-12-09 19:56:47 +0300

    Update StrTest5.java

--- src/test/java/junit/str/StrTest5.java
+++ src/test/java/junit/str/StrTest5.java
@@ -8,12 +8,12 @@ public class StrTest5 {
 	//Given a string and a non-empty word string, return a string made of each char just before and just after every appearance of the word in the string.
 	//Ignore cases where there is no char before or after the word, and a char may be included twice if it is between two words.
 
+    private Str str = new Str();
+
 	@Test
 	public void shouldReturnRequiredResultWhenWord() {
 		String[] actual = {"XY123XY", "XY"};
 		String expected = "13";
-
-		Str str = new Str();
 		assertEquals(expected, str.wordEnds(actual[0], actual[1]));
 	}
 
@@ -21,8 +21,6 @@ public class StrTest5 {
 	public void shouldReturnRequiredResultWhenTwoWords() {
 		String[] actual = {"abcXY123XYijk", "XY"};
 		String expected = "c13i";
-
-		Str str = new Str();
 		assertEquals(expected, str.wordEnds(actual[0], actual[1]));
 	}
 
@@ -30,16 +28,12 @@ public class StrTest5 {
 	public void shouldReturnEmptyWhenEmpty() {
 		String[] actual = {"", "XY"};
 		String expected = "";
-
-		Str str = new Str();
 		assertEquals(expected, str.wordEnds(actual[0], actual[1]));
 	}
 
 	@Test(expected = ArrayIndexOutOfBoundsException.class)
 	public void shouldReturnArrayIndexOutOfBoundsExceptionWhenNoWord() {
 		String[] actual = {"asdf"};
-
-		Str str = new Str();
 		str.wordEnds(actual[0], actual[1]);
 	}
 
@@ -47,8 +41,6 @@ public class StrTest5 {
 	@Test(expected = NullPointerException.class)
 	public void shouldReturnNullPointerExceptionWhenNull() {
 		String actual[] = null;
-
-		Str str = new Str();
 		str.wordEnds(actual[0], actual[1]);
 	}
 
