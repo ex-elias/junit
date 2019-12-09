fc61ab23f390931ed8f27d2396cf71afdee02bcf
jexidas <ilshelkov@ya.ru>
2019-12-09 19:55:37 +0300

    Update StrTest2.java

--- src/test/java/junit/str/StrTest2.java
+++ src/test/java/junit/str/StrTest2.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 public class StrTest2 {
 	//Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
 
+    private Str str = new Str();
+
 	@Test
 	public void shouldReturnRequiredResultWhenStartsWithXX() {
 		String actual = "xxHi";
 		String expected = "Hi";
-
-		Str str = new Str();
 		assertEquals(expected, str.withoutX2(actual));
 	}
 
@@ -20,8 +20,6 @@ public class StrTest2 {
 	public void shouldReturnRequiredResultWhenXIsSecond() {
 		String actual = "QxHi";
 		String expected = "QHi";
-
-		Str str = new Str();
 		assertEquals(expected, str.withoutX2(actual));
 	}
 
@@ -29,8 +27,6 @@ public class StrTest2 {
 	public void shouldReturnRequiredResultWhenNoXAtBeginning() {
 		String actual = "HiQQ";
 		String expected = "HiQQ";
-
-		Str str = new Str();
 		assertEquals(expected, str.withoutX2(actual));
 	}
 
@@ -38,16 +34,12 @@ public class StrTest2 {
 	public void shouldReturnEmptyWhenEmpty() {
 		String actual = "";
 		String expected = "";
-
-		Str str = new Str();
 		assertEquals(expected, str.withoutX2(actual));
 	}
 
 	@Test(expected = NullPointerException.class)
 	public void shouldReturnNullPointerExceptionWhenNull() {
 		String actual = null;
-
-		Str str = new Str();
 		str.withoutX2(actual);
 	}
 
